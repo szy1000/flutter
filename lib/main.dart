b9fc3d3a9f016b2007f89b3da6c98dae4d1b4ecb
szy1000 <1538078053@qq.com>
2021-07-09 16:17:32 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // Try running your application with "flutter run". You'll see the
+  //       // application has a blue toolbar. Then, without quitting the app, try
+  //       // changing the primarySwatch below to Colors.green and then invoke
+  //       // "hot reload" (press "r" in the console where you ran "flutter run",
+  //       // or simply save your changes to "hot reload" in a Flutter IDE).
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // is not restarted.
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: MyHomePage(title: 'Flutter Demo Home Page'),
+  //   );
+  // }
+
+
+
   @override
   Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+        title: 'demo',
+        home: Scaffold(
+          // appBar: AppBar(
+          //   title: Text('Welcome to Flutter!'),
+          // ),
+          body: Column(
+            children: [
+              Container(
+                child:  RandomWords(),
+                height: 700,
+              ),
+              Container(
+                child: Text('tabBar'),
+                height: 44.0,
+                width: double.infinity,
+                color: Colors.blue,
+              ),
+
+            ],
+
+            //     child: Container(
+          //   child: new Text(
+          //     wordPair.asPascalCase,
+          //     // '本期重点关注该区单数(Singular)互博国际足球平台的热出，与其委屈的恋爱，不如骄傲的单身奇数互博国际足球平台',
+          //     textAlign: TextAlign.center,
+          //     maxLines: 2,
+          //     overflow: TextOverflow.fade,
+          //   ),
+          //   width: 300.0,
+          //   height: 400.0,
+          //   color: Colors.lightBlue,
+          //   alignment: Alignment.center,
+          // )
+          ),
+        )
     );
   }
 }
+
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+              body: new ListView(children: divided,)
+          );
+
+        }
+      )
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if(i.isOdd) return new Divider();
+
+        final index = i ~/2;
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
+    // return Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget> [
+          new IconButton(
+            icon: new Icon(Icons.list),
+            onPressed: _pushSaved
+          )
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+    // return _buildSuggestions();
+  }
+}
+
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
